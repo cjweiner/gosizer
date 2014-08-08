816b44f8897abe1b4db140ac2a0d3ddd2b17ff66
cjweiner <cjweiner@gmail.com>
2014-08-08 10:59:58 -0400

    created rough layout for application and removed cover.css

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
 (function(){
 	var App = Ember.Application.create();
+
+	Ember.Handlebars.registerBoundHelper('currentDate', function() {
+		var currentTime = new Date()
+  		return currentTime.getFullYear();
+	});
 })();
--- server/server.go
+++ server/server.go
@@ -13,7 +13,11 @@ type Image struct {
 	Extension string
 }
 
-func GetResizedImage(w http.ResponseWriter, r *http.Request) {
+func OriginalImageHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ResizedImageHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
@@ -21,6 +25,8 @@ func main() {
 	log.Println("Starting Server...")
 	router := mux.NewRouter()
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../public/")))
+	router.HandleFunc("/api/image", OriginalImageHandler).Methods("POST")
+	router.HandleFunc("/api/image", ResizedImageHandler).Methods("GET")
 	n := negroni.Classic()
 	n.UseHandler(router)
 	n.Run(":8080")
